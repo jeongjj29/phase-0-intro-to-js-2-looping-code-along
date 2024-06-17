d023a43b2820ab8b1b390d02afa708f4340e3d42
Jae Jeong <jeongjj29@gmail.com>
2024-06-17 03:57:21 -0400

    adds for/while functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const greetings = [];
+    for (let i = 0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return greetings;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--;
+    }
+}
